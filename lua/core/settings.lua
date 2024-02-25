822c38b2cca8b81c69916d407f40571838dd3602
Zetaniis <63559030+Zetaniis@users.noreply.github.com>
2024-02-25 12:24:04 +0100

    Change timeoutlen and updatetime values

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -24,8 +24,8 @@ vim.o.smartcase = true
 vim.wo.signcolumn = 'yes'
 
 -- Decrease update time, TODO not sure what values should I use 
--- vim.o.updatetime = 250
--- vim.o.timeoutlen = 300
+vim.o.updatetime = 1000
+vim.o.timeoutlen = 300
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'

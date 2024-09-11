c9fe1998f58ab3d2f693d2aa202586226e5e54fc
Zetaniis <63559030+Zetaniis@users.noreply.github.com>
2024-09-11 19:28:11 +0200

    Misc fixes and small theme switcher

---
+++ lua/plugins/rust.lua
@@ -0,0 +1,9 @@
+return {
+    -- {
+    --     'mrcjkb/rustaceanvim',
+    --     version = '^3', -- Recommended
+    --     ft = { 'rust' },
+    -- },
+
+}
+
---
+++ lua/plugins/sleuth.lua
@@ -0,0 +1,4 @@
+return {
+    -- TODO figure what it it for and how to use it
+    -- {tpope/vim-sleuth},
+}
